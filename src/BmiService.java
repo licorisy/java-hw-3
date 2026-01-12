12793ba2aa5eba929efa121e1f859a8aa069ea24
licoris <klepehin.k.a@gmail.com>
2026-01-12 15:18:17 +0300

    deletmain2andBmiservice

--- src/BmiService.java
+++
@@ -1,7 +0,0 @@
-public class BmiService {
-    public double calculate(double height, double weight) {
-        double result = weight / (height * height);
-        int trueResult = (int) result;
-        return trueResult;
-    }
-}
